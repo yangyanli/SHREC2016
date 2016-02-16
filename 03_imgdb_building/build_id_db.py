b385cee60b10388c5e722132dcefed822ab11d5b
Yangyan Li <yangyan@stanford.edu>
2016-02-16 22:01:06 +0200

    Add script for building id lmdb.

---
+++ 03_imgdb_building/build_id_db.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import lmdb
+import numpy as np
+
+#https://github.com/BVLC/caffe/issues/861#issuecomment-70124809
+import matplotlib 
+matplotlib.use('Agg')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(BASE_DIR))
+from global_variables import *
+
+sys.path.append(os.path.join(g_caffe_installation_path, 'python'))
+import caffe
+    
+datasets = ['train', 'val']
+perturbs = ['', '_perturbed']
+view_num = 12
+for i in range(view_num+1):
+  for dataset in datasets:
+    for perturb in perturbs:
+      view_str = '_view_%02d' % (i)
+      if i == view_num:
+          view_str = ''
+      id_filename = os.path.join(g_imgdb_building_folder, '%s%s_id.txt'%(dataset+perturb, view_str))
+      id_folder = os.path.join(g_imgdb_building_folder, '%s%s_id_lmdb' % (dataset+perturb, view_str))
+      
+      env = lmdb.open(id_folder, map_size=int(1e12))
+      ids = [int(line) for line in open(id_filename, 'r')]
+      array = [None]
+      with env.begin(write=True) as txn:
+        for category_id in ids:
+          array[0] = category_id
+          datum = caffe.io.array_to_datum(np.asarray(array).astype(float), category_id)
+          txn.put('{:0>10d}'.format(start_idx+idx), datum.SerializeToString())
+      env.close();
