1c3956a6b3bd631f1303430fcf4bc675348264f6
Yangyan Li <yangyan@stanford.edu>
2016-02-16 19:42:17 +0800

    Fix lmdb building code.

--- 03_imgdb_building/build_image_db.py
+++ 03_imgdb_building/build_image_db.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import time
 from subprocess import call
 from subprocess import Popen
 
@@ -27,17 +28,18 @@ for i in range(view_num):
   for dataset in datasets:
     for perturb in perturbs:
       image_filelist = os.path.join(g_imgdb_building_folder, '%s_view_%02d_path_subid.txt'%(dataset+perturb, i))
-      imagedb_folder = os.path.join(g_imgdb_building_folder, '%s_view_%02d' % (dataset+perturb, i))
+      imagedb_folder = os.path.join(g_imgdb_building_folder, '%s_view_%02d_lmdb' % (dataset+perturb, i))
       args = [convert_imageset_executable_path, '-resize_height', '227', '-resize_width', '227', '/', image_filelist, imagedb_folder]
       log_filename = os.path.join(g_imgdb_building_folder, '%s_view_%02d_log.txt'%(dataset+perturb, i))
       with open(log_filename, 'w') as log_file:
-        processes.append(Popen(args, stdout=log_file, stderr=log_file)
+        processes.append(Popen(args, stdout=log_file, stderr=log_file))
+        args_list.append(args)
       while len(processes) >= g_imgdb_building_thread_num:
         for p in processes:
-          if p.poll() is None:
+          if p.poll() is not None:
             p_idx = processes.index(p)
             if p.returncode != 0:
-              print 'Error: command \'%s\' failed!!' % (args_list[p_idx])   
+              print 'Error: command \'%s\' failed!!' % (' '.join(args_list[p_idx]))   
             del args_list[p_idx]
             processes.remove(p)
         time.sleep(1)
