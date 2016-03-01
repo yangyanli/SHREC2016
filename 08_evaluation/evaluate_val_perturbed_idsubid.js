246a150105b3df09bcdffaaaf2fa2da9d5b62012
Yangyan Li <yangyan@stanford.edu>
2016-03-01 22:51:06 +0800

    Add more results.

---
+++ 08_evaluation/evaluate_val_perturbed_idsubid.js
@@ -0,0 +1,3 @@
+var Evaluator = require('./Evaluator.js');
+var evaluator = new Evaluator({truthFile: '/home/yangyan/SHREC2016/dataset_shape_retrieval/val.csv'});
+evaluator.evaluate('/home/yangyan/SHREC2016/dataset_shape_retrieval/pooling/val_perturbed/val_perturbed_idsubid/');
